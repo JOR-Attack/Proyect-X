0d0e7fb0c6cde394b7bb0faa0a14a71061568ad4
Miguel Jimenez <57577766+Zentrax997@users.noreply.github.com>
2023-07-10 15:55:18 -0500

    Add files via upload

---
+++ PROJECTX/src/main/java/com/mycompany/projectx/PROJECTX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectx;
+
+/**
+ *
+ * @author MIKE
+ */
+public class PROJECTX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
